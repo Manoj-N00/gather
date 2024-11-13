c3e1f86b81feae1195855118396954687b0bcf28
Manoj-N00 <manojnaikj@gmail.com>
2024-11-13 15:10:03 +0530

    http layer

--- gather/apps/http/src/routes/v1/index.ts
+++ gather/apps/http/src/routes/v1/index.ts
@@ -1,16 +1,34 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { adminRouter } from "./admin";
+import { spaceRouter } from "./space";
 
  export const router =Router();
 
- router.get("/signup",(req,res)=>{
+ router.post("/signup",(req,res)=>{
     res.json({
         message:"signup"
     })
  }
 )
- router.get("/signin",(req,res)=>{
+ router.post("/signin",(req,res)=>{
     res.json({
         message:"signin"
     })
 
  })
+
+ router.get("/elements",(req,res)=>{
+    res.json({
+        message:"elements"
+    })
+ })
+ router.get("/avatars",(req,res)=>{
+    res.json({
+        message:"avatas"
+    })
+ })
+
+ router.use("/user",userRouter);
+ router.use("/space",spaceRouter);
+ router.use("/admin",adminRouter);
