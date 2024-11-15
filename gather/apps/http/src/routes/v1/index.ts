c2589a890bcd87801ea1cff8c626b030eb54d407
Manoj-N00 <manojnaikj@gmail.com>
2024-11-15 19:01:54 +0530

    signup function

--- gather/apps/http/src/routes/v1/index.ts
+++ gather/apps/http/src/routes/v1/index.ts
@@ -2,13 +2,37 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
+import { SignupSchema } from "../../middleware/admin";
+import client from "@repo/db/client";
 
  export const router =Router();
 
- router.post("/signup",(req,res)=>{
-    res.json({
-        message:"signup"
-    })
+ router.post("/signup",async(req,res)=>{
+    const parsedData = SignupSchema.safeParse(req.body);
+    if(!parsedData.success){
+         res.status(400).json({
+            message :"Validation failed"
+        })
+        return
+    }
+    try{
+     const user = await client.user.create({
+            data:{
+                username:parsedData.data.username,
+                password:parsedData.data.password,
+                role:parsedData.data.type==="admin"?"Admin":"User"
+            }
+        })
+        res.json({
+           userId:user.id
+        })
+
+    }catch(e){
+        res.status(400).json({
+            message:"User already exists"
+        })
+
+    }
  }
 )
  router.post("/signin",(req,res)=>{
