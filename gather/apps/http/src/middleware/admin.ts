20fabcd92959dc48e1a8fc1cbf50ac350ef8663a
Manoj-N00 <manojnaikj@gmail.com>
2024-11-16 13:21:20 +0530

    avatars

--- gather/apps/http/src/middleware/admin.ts
+++ gather/apps/http/src/middleware/admin.ts
@@ -1,57 +1,30 @@
-import z from "zod";
+import jwt from "jsonwebtoken";
+  import { JWT_PASSWORD } from "../config";
+  import { NextFunction, Request, Response } from "express";
+export const adminMiddleware =  async (req:Request,res:Response,next:NextFunction)=>{
+    const header = req.headers.authorization
+    const token = header?.split(" ")[1]
 
- export const SignupSchema = z.object({
-  username: z.string().email(),
-  password: z.string().min(8),
-  type: z.enum(["admin", "user"]),
-});
-
-export const SigninSchema = z.object({
-  username: z.string().email(),
-  password: z.string().min(8),
-});
-
-export const UpdateMetadata = z.object({
-  avatarId: z.string(),
-});
-
-export const createSpaceSchema = z.object({
-  name: z.string(),
-  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
-  mapId: z.string(),
-});
-
-export const addElementSchema = z.object({
-  elementId: z.string(),
-  spaceId: z.string(),
-  x: z.number(),
-  y: z.number(),
-});
-
-export const createElementSchema = z.object({
-  imageUrl: z.string(),
-  width: z.number(),
-  height: z.number(),
-  static: z.boolean(),
-}); 
-
-export const updateElementSchema = z.object({
-  imageUrl: z.string(),
-});
-
-export const createAvatarSchema = z.object({
-  imageUrl: z.string(),
-  name: z.string(),
-});
-
-export const createMapSchema = z.object({
-  thumbnail: z.string(),
-  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
-  defaultElements: z.array(
-    z.object({
-      elementId: z.string(),
-      x: z.number(),
-      y: z.number(),
-    })
-  ),
-});
+    if(!token){
+        res.status(403).json({
+            message:"Invalid token"
+        })
+        return
+    }
+    try{ 
+        const decoded = jwt.verify(token,JWT_PASSWORD) as {role:string,userId:string}
+        if(decoded.role!=="Admin"){
+            res.status(403).json({
+                message:"Invalid token"
+            })
+            return
+        }
+        req.userId=decoded.userId
+        next()
+    }catch(e){
+        res.status(401).json({
+            message:"Invalid token"
+        })
+    }
+        
+}
--- gather/apps/http/src/routes/v1/user.ts
+++ gather/apps/http/src/routes/v1/user.ts
@@ -1,11 +1,49 @@
 import { Router } from "express";
+import { UpdateMetadata} from "../../types";
+import client from "@repo/db/client";
+import { userMiddleware } from "../../middleware/user";
 
  export const userRouter=Router();
 
-userRouter.post("/metadata",(req,res)=>{
-
+userRouter.post("/metadata", userMiddleware,async(req,res)=>{
+const parsedData = UpdateMetadata.safeParse(req.body);
+if(!parsedData.success){
+    res.status(400).json({message:"Validation failed"})
+    return
+}
+await client.user.update({
+    where:{
+        id:req.userId 
+    },
+    data:{
+        avatarId:parsedData.data.avatarId
+    }
+})
+res.json({
+    message:"Metadata updated"
+})
 })
 
-userRouter.get("/metadata/bulk",(req,res)=>{
+userRouter.get("/metadata/bulk",async(req,res)=>{
+    const userIdString = (req.query.ids ?? "[]") as string;
+    const userIds = (userIdString).slice(1, userIdString?.length - 1).split(",");
+    const metadata = await client.user.findMany({
+        where: {
+            id: {
+                in: userIds
+            }
+        },select:{
+            avatar:true,
+            id:true
+        }
+    })
+    res.json({
+        avatars:metadata.map(x=>{
+            return {
+                userId:x.id,
+                avatarId:x.avatar?.imageUrl
+            }
+        })
+    })
 
 })
